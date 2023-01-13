bfe204f4fdb693082fbc2ea20eadbe3fb7978acc
kkamara <kamaracomputers@gmail.com>
2023-01-13 12:02:44 +0000

    Update migration: user id

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
