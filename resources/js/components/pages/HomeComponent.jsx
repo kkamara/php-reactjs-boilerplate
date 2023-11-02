b810c144ab70105bb9551209c57af24ba155bb8b
kkamara <kamaracomputers@gmail.com>
2023-11-02 19:48:01 +0000

    Request Users Data In Reactjs ComponentDidMount

--- resources/js/components/pages/HomeComponent.jsx
+++ resources/js/components/pages/HomeComponent.jsx
@@ -19,6 +19,7 @@ export default function HomeComponent() {
 
   useEffect(() => {
     dispatch(authorize())
+    dispatch(getUsers())
   }, [])
 
   useEffect(() => {
@@ -26,7 +27,6 @@ export default function HomeComponent() {
       localStorage.removeItem('user-token')
       window.location.href = '/user/login'
     }
-    dispatch(getUsers())
   }, [state.auth,])
 
   const handlePageChange = ({ selected, }) => {
