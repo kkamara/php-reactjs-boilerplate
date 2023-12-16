f2a18d8359f044273c967bc46673acc56db118b8
kkamara <kamaracomputers@gmail.com>
2023-12-16 17:00:10 +0000

    Redesign homepage

--- resources/js/components/pages/HomeComponent.jsx
+++ resources/js/components/pages/HomeComponent.jsx
@@ -39,26 +39,60 @@ export default function HomeComponent() {
 
   const parseDate = date => moment(date).format('YYYY-MM-DD hh:mm')
 
+  const pagination = () => {
+    if (!state.users.data) {
+        return null
+    }
+
+    return <ReactPaginate
+        onPageChange={handlePageChange}
+        previousLabel="Previous"
+        nextLabel="Next"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakLabel="..."
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        pageCount={state.users.data.last_page}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        containerClassName="pagination"
+        activeClassName="active"
+        forcePage={state.users.data.current_page - 1}
+    />
+  }
+
+  const paginationDetail = () => {
+        return <>
+            <strong>page</strong> ({state.users.data.current_page}),
+            <strong>page_count</strong> ({state.users.data.last_page}),
+            <strong>displayed_items</strong> ({state.users.data.data.length}),
+            <strong>items</strong> ({state.users.data.total})
+        </>
+  }
+
   const renderList = () => {
     if (!state.users.data) {
       return null
     }
     return (
       <>
+        {paginationDetail()}
         <ul className="list-group">
           {state.users.data.data.map((user, index) => (
             <li key={index} className='list-group-item home-item'>
-              <strong>name</strong> ({user.first_name} {user.last_name}), 
+              <strong>name</strong> ({user.first_name} {user.last_name}),
               <strong>email</strong> ({user.email}),
               <strong>created_at</strong> ({parseDate(user.created_at)}),
               <strong>updated_at</strong> ({parseDate(user.updated_at)})
             </li>
           ))}
         </ul>
-        <strong>page</strong> ({state.users.data.current_page}),
-        <strong>page_count</strong> ({state.users.data.last_page}),
-        <strong>displayed_items</strong> ({state.users.data.data.length}),
-        <strong>items</strong> ({state.users.data.total})
+        {paginationDetail()}
       </>
     )
   }
@@ -78,37 +112,15 @@ export default function HomeComponent() {
       <div className='container'>
         <br />
         <br />
-        {renderList()}
-        <br />
-        <br />
         <button className='btn btn-primary'>
           Test button
         </button>
-        {state.users.data ?
-          <>
-            <br />
-            <ReactPaginate
-              onPageChange={handlePageChange}
-              previousLabel="Previous"
-              nextLabel="Next"
-              pageClassName="page-item"
-              pageLinkClassName="page-link"
-              previousClassName="page-item"
-              previousLinkClassName="page-link"
-              nextClassName="page-item"
-              nextLinkClassName="page-link"
-              breakLabel="..."
-              breakClassName="page-item"
-              breakLinkClassName="page-link"
-              pageCount={state.users.data.last_page}
-              marginPagesDisplayed={2}
-              pageRangeDisplayed={5}
-              containerClassName="pagination"
-              activeClassName="active"
-              forcePage={state.users.data.current_page - 1}
-            />
-          </> : null}
+        <br />
+        <br />
+        {pagination()}
+        {renderList()}
+        {pagination()}
       </div>
-    </>       
+    </>
   )
 }
