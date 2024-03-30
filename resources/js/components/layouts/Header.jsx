7ac5714112389abcd8f17236fa587e06f3be7040
kkamara <kamaracomputers@gmail.com>
2024-03-30 15:54:18 +0000

    Use Link for navbar links

--- resources/js/components/layouts/Header.jsx
+++ resources/js/components/layouts/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector, } from 'react-redux'
+import { Link, } from 'react-router-dom'
 
 export default function Header(props) {  
   const authResponse = useSelector(state=>state.auth)
@@ -12,34 +13,34 @@ export default function Header(props) {
         </a>
         <ul className="dropdown-menu">
           <li>
-            <a 
+            <Link
               className="dropdown-item" 
-              href="/user/logout"
+              to="/user/logout"
             >
               Logout
-            </a>
+            </Link>
           </li>              
         </ul>
       </li>
     } else {
       return <>
         <li className="nav-item">
-          <a 
+          <Link
             className="nav-link active" 
             aria-current="page" 
-            href="/user/login"
+            to="/user/login"
           >
             Login
-          </a>
+          </Link>
         </li>
         <li className="nav-item">
-          <a 
+          <Link
             className="nav-link active" 
             aria-current="page" 
-            href="/user/register"
+            to="/user/register"
           >
             Register
-          </a>
+          </Link>
         </li>
       </>
     }
@@ -48,14 +49,14 @@ export default function Header(props) {
   console.log(import.meta.env)
   return <nav className="container navbar navbar-expand-lg bg-body-tertiary">
     <div className="container-fluid">
-      <a className="navbar-brand" href="/">PHP Reactjs Boilerplate</a>
+      <Link className="navbar-brand" to="/">PHP Reactjs Boilerplate</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/">Home</a>
+            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
           </li>
         </ul>
         <ul className="navbar-nav">
