3816f2a89e49063cb82ed45e85787372693d9248
kkamara <kamaracomputers@gmail.com>
2024-11-17 13:13:26 +0000

    Use HTTPS in app footer

--- resources/js/components/layouts/Footer.jsx
+++ resources/js/components/layouts/Footer.jsx
@@ -5,7 +5,7 @@ import "./Footer.scss"
 export default function Footer() {
   return <footer className="app-footer mt-4 text-center">
     <a 
-      href="http://www.github.com/kkamara"
+      href="https://www.github.com/kkamara"
       className="btn btn-lg btn-default"
     >
       www.kelvinkamara.com
