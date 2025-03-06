cb97c0a8f3131d7a2607933314db7484477f215c
kkamara <kamaracomputers@gmail.com>
2025-03-06 18:01:29 +0000

    Update code formatting for routes files

--- routes/api.php
+++ routes/api.php
@@ -4,36 +4,41 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Web\UserController as WebUserController;
 use App\Http\Controllers\API\UserController;
 
+// Add single page app API routes
 Route::prefix("web")
     ->group(function () {
-        // Add single page app api routes
         Route::prefix("/user")->group(function () {
             Route::post("/register", [WebUserController::class, "register"]);
             Route::post("/", [WebUserController::class, "login"]);
             Route::delete(
-                "/logout",
-                [WebUserController::class, "logout"],
-            )->middleware("auth:sanctum");
+                    "/logout",
+                    [WebUserController::class, "logout"],
+                )
+                ->middleware("auth:sanctum");
             Route::get(
-                "/authorize",
-                [WebUserController::class, "authorizeUser"],
-            )->middleware("auth:sanctum");
+                    "/authorize",
+                    [WebUserController::class, "authorizeUser"],
+                )
+                ->middleware("auth:sanctum");
         });
         Route::get(
-            "/users",
-            [WebUserController::class, "getUsers"],
-        )->middleware("auth:sanctum");
+                "/users",
+                [WebUserController::class, "getUsers"],
+            )
+            ->middleware("auth:sanctum");
     });
-
+// Add third-party API routes
 Route::prefix("/user")->group(function () {
     Route::post("/register", [UserController::class, "register"]);
     Route::post("/", [UserController::class, "login"])->name("login");
     Route::delete(
-        "/logout",
-        [UserController::class, "logout"],
-    )->middleware("auth:sanctum");
+            "/logout",
+            [UserController::class, "logout"],
+        )
+        ->middleware("auth:sanctum");
     Route::get(
-        "/authorize",
-        [UserController::class, "authorizeUser"],
-    )->middleware("auth:sanctum");
+            "/authorize",
+            [UserController::class, "authorizeUser"],
+        )
+        ->middleware("auth:sanctum");
 });
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view("/{path?}", "layouts.app")->where("path", ".*");
+Route::view("/{path?}", "layouts.app")
+    ->where("path", ".*");
