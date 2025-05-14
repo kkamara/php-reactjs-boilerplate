af53fe3c7c6ba6979ee3aebccddcb1f1a7b7391d
kkamara <kamaracomputers@gmail.com>
2025-05-14 19:05:47 +0100

    Update logout for maximum update depth exceeded error

--- resources/js/components/pages/auth/LogoutComponent.jsx
+++ resources/js/components/pages/auth/LogoutComponent.jsx
@@ -1,32 +1,15 @@
 import React, { useEffect, } from "react"
-import { useNavigate, } from "react-router-dom"
 import { useDispatch, useSelector, } from "react-redux"
 import { Helmet, } from "react-helmet"
 import { logout, } from "../../../redux/actions/authActions"
 
 export default function LogoutComponent() {
-  const navigate = useNavigate()
-
   const dispatch = useDispatch()
   const authState = useSelector(state => state.auth)
 
-  useEffect(() => {}, [])
-
-  let token
-
   useEffect(() => {
-    token = localStorage.getItem("user-token")
-    if (token !== null) {
-      dispatch(logout())
-    }
-  })
-
-  useEffect(() => {
-    token = localStorage.getItem("user-token")
-    if (token === null) {
-      return navigate("/user/login")
-    }
-  }, [authState])
+    dispatch(logout())
+  }, [])
 
   if (authState.loading) {
     return <div className="container logout-container text-center">
