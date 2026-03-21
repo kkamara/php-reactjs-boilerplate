21eebf3bea6e72fd157fe194ec4215e9df0ceaf3
kkamara <kamaracomputers@gmail.com>
2026-03-21 10:36:22 +0000

    Update token path handling from response (2)

--- resources/js/services/AuthService.js
+++ resources/js/services/AuthService.js
@@ -18,7 +18,7 @@ export const LoginUserService = (credentials) => {
   return http.getData(http.domain+"/sanctum/csrf-cookie").then(() =>
     http.postData("/user", credentials)
       .then(response => {
-        localStorage.setItem(tokenID, response.data.user.token)
+        localStorage.setItem(tokenID, response.data.data.user.token)
         return response.data
       })
       .catch(err => { throw err })
